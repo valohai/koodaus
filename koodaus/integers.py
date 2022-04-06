ef9422ee382e33699dc63ea3d4cc0c81c562227c
Aarni Koskela <akx@iki.fi>
2022-04-06 11:31:06 +0300

    Add type annotations

--- koodaus/integers.py
+++ koodaus/integers.py
@@ -1,45 +1,41 @@
 import string
 
-keyspace = string.digits + string.ascii_letters
+DEFAULT_KEYSPACE = string.digits + string.ascii_letters
 
 
-def encode_int(value, keyspace=keyspace):
+def encode_int(value: int, keyspace: str = DEFAULT_KEYSPACE) -> str:
     """
     Encode a positive integer into a variable-length string.
 
     By default, the keyspace is ascii-compatible.
 
     :param value: The integer to encode.
-    :type value: int
     :param keyspace: The keyspace string to use.
-    :type keyspace: str
     :return: Encoded string.
     """
     if value < 0:
         raise ValueError("value must be positive")
     value = int(value)
     out = []
+    n = len(keyspace)
     while value:
-        value, digit = divmod(value, len(keyspace))
+        value, digit = divmod(value, n)
         out.append(keyspace[digit])
     return "".join(out[::-1])
 
 
-def decode_int(str, keyspace=keyspace):
+def decode_int(value: str, keyspace: str = DEFAULT_KEYSPACE) -> int:
     """
     Decode an integer previously encoded by `decode_int`.
 
-    :param str: The encoded string.
-    :type str: str
+    :param value: The encoded string.
     :param keyspace: The encoding keyspace. This will naturally
                      have to be the same used while encoding for
                      correct results.
-    :type keyspace: str
     :return: an integer
-    :rtype: int
     """
     out = 0
     n = len(keyspace)
-    for char in str:
+    for char in value:
         out = out * n + keyspace.index(char)
     return out
